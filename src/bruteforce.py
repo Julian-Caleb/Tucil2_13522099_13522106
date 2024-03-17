cb4ff3d6bb200cbf2472575d12c57e67f5b9ce3c
ChristopherBrian <cbnnolan@gmail.com>
2024-03-17 21:12:56 +0700

    changed elapsed time

--- src/bruteforce.py
+++ src/bruteforce.py
@@ -1,13 +1,18 @@
 import matplotlib.pyplot as plt
 import math
 from typing import List, Tuple
+import time
 
 # Fungsi untuk mendapatkan koefisien berdasarkan segitiga Pascal (Pascal's Triangle)
 def pt(i, j):
     return math.comb(i - 1, j - 1)
 
+# Fungsi untuk menunjukkan koordinat titik sebagai teks
+def point_text(point_name: str, point: (float, float)):
+    plt.text(point_name, point[0], point[1], f"{point_name} ({point[0]:.3f}, {point[1]:.3f})", fontsize=8, ha='center', va='bottom')
+
 # Fungsi untuk menampilkan garis
-def draw_line(p0 : (float, float), p1 : (float, float)) :
+def draw_line(p0 : (float, float), p1 : (float, float)):
     arr = []
     arr.append(p0)
     arr.append(p1)
@@ -15,9 +20,12 @@ def draw_line(p0 : (float, float), p1 : (float, float)) :
     y = [point[1] for point in arr]
     plt.plot(x, y, marker = 'o', linestyle = '-', color = "red")
 
+    for point in arr:
+        plt.text(point[0], point[1], f"({point[0]:.3f}, {point[1]:.3f})", fontsize=8, ha='center', va='bottom')
+
 # Fungsi rekursif untuk membuat kurva bezier
-# Iteration juga digunakan untuk warnanya
-def draw_bezier_curve_bf(control_points: List[Tuple[float, float]], iteration: int) :
+def draw_bezier_curve_bf(control_points: List[Tuple[float, float]], iteration: int):
+    start_time = time.time()
     arr_of_curve_points = []
     num_control_points = len(control_points)
     if (iteration > 0) :
@@ -31,3 +39,11 @@ def draw_bezier_curve_bf(control_points: List[Tuple[float, float]], iteration: i
             arr_of_curve_points.append((x, y))
         for k in range(iteration):
             draw_line(arr_of_curve_points[k], arr_of_curve_points[k + 1])
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        plt.gca().set_aspect('equal', adjustable='box')
+        x_min, x_max = plt.xlim()
+        y_min, y_max = plt.ylim()
+        center_x = (x_min + x_max) / 2
+        center_y = y_max
+        plt.text(center_x, center_y, f"Elapsed time: {elapsed_time:.4f} seconds", fontsize=12, ha='center')
