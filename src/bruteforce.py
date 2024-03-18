3c71fc9281b51b5e54f190af2c8a37902ff2ac84
ChristopherBrian <cbnnolan@gmail.com>
2024-03-18 23:24:13 +0700

    changed bruteforce.py

--- src/bruteforce.py
+++ src/bruteforce.py
@@ -3,14 +3,6 @@ import math
 from typing import List, Tuple
 import time
 
-# Fungsi untuk mendapatkan koefisien berdasarkan segitiga Pascal (Pascal's Triangle)
-def pt(i, j):
-    return math.comb(i - 1, j - 1)
-
-# Fungsi untuk menunjukkan koordinat titik sebagai teks
-def point_text(point_name: str, point: (float, float)):
-    plt.text(point_name, point[0], point[1], f"{point_name} ({point[0]:.3f}, {point[1]:.3f})", fontsize=8, ha='center', va='bottom')
-
 # Fungsi untuk menampilkan garis
 def draw_line(p0 : (float, float), p1 : (float, float)):
     arr = []
@@ -19,9 +11,6 @@ def draw_line(p0 : (float, float), p1 : (float, float)):
     x = [point[0] for point in arr]
     y = [point[1] for point in arr]
     plt.plot(x, y, marker = 'o', linestyle = '-', color = "red")
-
-    for point in arr:
-        plt.text(point[0], point[1], f"({point[0]:.3f}, {point[1]:.3f})", fontsize=8, ha='center', va='bottom')
 
 # Fungsi rekursif untuk membuat kurva bezier
 def draw_bezier_curve_bf(control_points: List[Tuple[float, float]], iteration: int):
@@ -34,8 +23,8 @@ def draw_bezier_curve_bf(control_points: List[Tuple[float, float]], iteration: i
             x = 0.0
             y = 0.0
             for j in range(num_control_points):
-                x += float(pt(num_control_points, j + 1) * ((1 - t * i) ** (num_control_points - 1 - j)) * ((t * i) ** j) * control_points[j][0])
-                y += float(pt(num_control_points, j + 1) * ((1 - t * i) ** (num_control_points - 1 - j)) * ((t * i) ** j) * control_points[j][1])
+                x += float(math.comb(num_control_points - 1, j) * ((1 - t * i) ** (num_control_points - 1 - j)) * ((t * i) ** j) * control_points[j][0])
+                y += float(math.comb(num_control_points - 1, j) * ((1 - t * i) ** (num_control_points - 1 - j)) * ((t * i) ** j) * control_points[j][1])
             arr_of_curve_points.append((x, y))
         for k in range(iteration):
             draw_line(arr_of_curve_points[k], arr_of_curve_points[k + 1])
